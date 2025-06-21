6dc09d20603a01524be267f22434168e6e453a31
amysidra <amysidra@gmail.com>
2025-06-21 17:01:41 +0700

    feat: add basic footer component with copyright text

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="w-full bg-white shadow text-center py-3 text-xs text-gray-500 mt-auto">
-      &copy; {new Date().getFullYear()} SMARTSCHOOL. All rights reserved.
+      &copy; {new Date().getFullYear()} Project Amy. All rights reserved.
     </footer>
   );
 }
